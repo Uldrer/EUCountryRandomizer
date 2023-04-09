4f6e44e556d05b6aec70b865c48ba960c00d69c3
Martin <uldrer@gmail.com>
2023-04-09 12:17:49 +0200

    Adding free and subjugated countries.

--- EUMissionAnalysis/Program.cs
+++ EUMissionAnalysis/Program.cs
@@ -31,7 +31,7 @@ namespace EUMissionAnalysis
             string[] linesCountries = System.IO.File.ReadAllLines(dirPath + "\\" + "countries.txt");
 
             Dictionary<string, string> countryDict = new Dictionary<string, string>();
-
+            
             foreach (string line in linesCountries)
             {
                 // Use a tab to indent each line of the file.
@@ -45,10 +45,38 @@ namespace EUMissionAnalysis
                 }
             }
 
+            string[] linesPlayers = System.IO.File.ReadAllLines(dirPath + "\\" + fileName1);
+
+            Dictionary<string, string> playerDict = new Dictionary<string, string>();
+
+            int indexStart = 2;
+            int count = 0;
+            string playerName = "";
+            foreach (string line in linesPlayers)
+            {
+                if(count >= indexStart)
+                {
+                    if(count % 2 == 0)
+                    {
+                        playerName = line.Replace("\t","");
+                    }
+                    else
+                    {
+                        string playerId = line.Replace("\t", "");
+                        playerDict.Add(playerId, playerName.Replace("\"",""));
+                    }
+                }
+                count++;
+
+                if (count > 15) break;
+            }
+
+
             Dictionary<string, string> religionDict = new Dictionary<string, string>();
             Dictionary<string, string> provinceDict = new Dictionary<string, string>();
             List<string> freeCityList = new List<string>();
             Dictionary<string, int> provinceDiffDict = new Dictionary<string, int>();
+            Dictionary<string, string> overlordDict = new Dictionary<string, string>();
 
             // Handle several files over files if present
             if (args.Length > 1) 
@@ -56,9 +84,10 @@ namespace EUMissionAnalysis
                 Dictionary<string, string> tempReligionDict = new Dictionary<string, string>();
                 Dictionary<string, string> tempProvinceDict = new Dictionary<string, string>();
                 List<string> tempFreeCityList = new List<string>();
+                Dictionary<string, string> tempOverlordDict = new Dictionary<string, string>();
 
-                ParseSaveFile(dirPath, fileName1, countryDict, tempReligionDict, tempProvinceDict, tempFreeCityList);
-                ParseSaveFile(dirPath, fileName2, countryDict, religionDict, provinceDict, freeCityList);
+                ParseSaveFile(dirPath, fileName1, countryDict, tempReligionDict, tempProvinceDict, tempFreeCityList, tempOverlordDict);
+                ParseSaveFile(dirPath, fileName2, countryDict, religionDict, provinceDict, freeCityList, overlordDict);
 
                 foreach(var countryKey in provinceDict.Keys)
                 {
@@ -69,7 +98,7 @@ namespace EUMissionAnalysis
             }
             else
             {
-                ParseSaveFile(dirPath, fileName1, countryDict, religionDict, provinceDict, freeCityList);
+                ParseSaveFile(dirPath, fileName1, countryDict, religionDict, provinceDict, freeCityList, overlordDict);
             }
             
 
@@ -129,10 +158,54 @@ namespace EUMissionAnalysis
             }
             Console.WriteLine();
 
+            Console.WriteLine("Subject countries");
+            // Printouts subject countries
+            foreach (var country in countryDict.Keys)
+            {
+                if(overlordDict.ContainsKey(countryDict[country]))
+                {
+                    string overlord = overlordDict[countryDict[country]];
+                    if (playerDict.ContainsKey(overlord))
+                    {
+                        Console.WriteLine(country + " : " + playerDict[overlord]);
+                    }
+                    else
+                    {
+                        string noCitationOverlord = overlord.Replace("\"", "");
+                        if (countryDict.ContainsValue(noCitationOverlord))
+                        {
+                            string prettyOverlord = countryDict.FirstOrDefault(x => x.Value == noCitationOverlord).Key;
+                            Console.WriteLine(country + " : " + prettyOverlord);
+                        }
+                        else
+                        {
+                            Console.WriteLine(country + " : " + overlord);
+                        }
+                    }
+                }
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Free countries");
+            // Printouts free countries
+            foreach (var country in countryDict.Keys)
+            {
+                if (!overlordDict.ContainsKey(countryDict[country]))
+                {
+                    int numberOfProvinces = Int32.Parse(provinceDict[countryDict[country]]);
+
+                    if(numberOfProvinces > 0)
+                    {
+                        Console.WriteLine(country);
+                    }
+                }
+            }
+            Console.WriteLine();
+
             var stop2 = Console.ReadLine();
         }
 
-        public static void ParseSaveFile(string dirPath, string fileName, Dictionary<string, string> countryDict, Dictionary<string, string> religionDict, Dictionary<string, string> provinceDict, List<string> freeCityList)
+        public static void ParseSaveFile(string dirPath, string fileName, Dictionary<string, string> countryDict, Dictionary<string, string> religionDict, Dictionary<string, string> provinceDict, List<string> freeCityList, Dictionary<string, string> overlordDict)
         {
             string[] linesSave = System.IO.File.ReadAllLines(dirPath + "\\" + fileName);
             
@@ -144,6 +217,7 @@ namespace EUMissionAnalysis
                 string searchStringGovRank2 = "\t\thas_set_government_name=";
                 string searchStringFreeCity = "\t\tgovernment_name=\"german_free_city\"";
                 string searchStringReligion = "\t\treligion=";
+                string searchStringOverlord = "\t\toverlord=";
                 string searchStringCities = "\t\tnum_of_cities=";
                 string searchStringEnd = "\t}";
                 bool foundCountryTag = false;
@@ -195,7 +269,12 @@ namespace EUMissionAnalysis
                     }
                     else if(foundReligion)
                     {
-                        if (line.StartsWith(searchStringCities))
+                        if (line.StartsWith(searchStringOverlord))
+                        {
+                            // Correctus
+                            overlordDict.Add(country, line.Substring(searchStringOverlord.Length));
+                        }
+                        else if (line.StartsWith(searchStringCities))
                         {
                             // Correctus
                             provinceDict.Add(country, line.Substring(searchStringCities.Length));
